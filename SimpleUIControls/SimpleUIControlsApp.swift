167ef3e29ba9e3fd1a4a716e73a50bc49ab91e6e
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-01-26 14:08:14 +0100

    Initial Commit

---
+++ SimpleUIControls/SimpleUIControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleUIControlsApp.swift
+//  SimpleUIControls
+//
+//  Created by Fabian on 26.01.21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleUIControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
